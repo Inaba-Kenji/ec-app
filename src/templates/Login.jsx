09f85f9aa2c6a3a8bc04c62d8818875fc56a62d0
Inaba Kenji <k.inaba040332@gmail.com>
2021-01-10 16:03:51 +0900

    finish: feature-React-Redux9

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,7 +1,7 @@
+import { Dialpad } from "@material-ui/icons";
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {push} from "connected-react-router";
-import { signInAction } from "../reducks/users/actions";
+import { signIn } from "../reducks/users/operations";
 
 const Login = () => {
   const dispatch = useDispatch()
@@ -9,10 +9,7 @@ const Login = () => {
   return(
     <div>
       <h2>ログイン</h2>
-      <button onClick={() => {
-        dispatch(signInAction({uid: "00001", username: "torahack"}));
-        dispatch(push('/'))
-      }}>
+      <button onClick={() => dispatch(signIn())}>
         ログインする
       </button>
     </div>
